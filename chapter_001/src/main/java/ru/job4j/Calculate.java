434bea052977adf9464fd509ebfbd12e757d5eee
vgurianov <37j5k5a@mail.ru>
2018-04-08 22:36:23 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ * Calculate is a template class for "Hello world" app
+ *
+ * @author Vlad G.
+ */
+
 public class Calculate{
+	
+	/**
+	 * Main method.
+	 * Prints "Hello world!" via command line.
+	 */
 	public static void main(String[] args){
 		System.out.print("Hello World!");
 	}
