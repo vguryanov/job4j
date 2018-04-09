b7d3ee0b5a84f7eead8be9e32ea235a07f3e13d6
vgurianov <37j5k5a@mail.ru>
2018-04-09 16:22:55 +0300

    2. Импорт Maven проекта. [#31168]

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,8 +1,10 @@
 package ru.job4j;
 
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,16 +14,16 @@ import static org.junit.Assert.assertThat;
  * @author Vlad G.
  */
 public class CalculateTest {
-	
-	/**
-	* Test echo.	
-	*/ 
-	@Test
-	public void whenTakeNameThenTreeEchoPlusName() {
-		String input = "Vlad G.";
-		String expect = "Echo, echo, echo : Vlad G."; 
-		Calculate calc = new Calculate();
-		String result = calc.echo(input);
-		assertThat(result, is(expect));
-	} 
+
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Vlad G.";
+        String expect = "Echo, echo, echo : Vlad G.";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
