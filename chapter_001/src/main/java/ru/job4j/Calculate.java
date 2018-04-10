4434f393ae074f5351c3f2abd915488e58346a44
vgurianov <37j5k5a@mail.ru>
2018-04-11 00:43:51 +0300

    1.3. JavaDoc. Code convention[#48774]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,25 +3,28 @@ package ru.job4j;
 /**
  * Calculate is a template class for "Hello world" application
  *
- * @author Vlad G.
+ * @author  Vlad G.
  */
 
 public class Calculate {
-	
-	/**
-	 * Main method.
-	 * Prints "Hello world!" via command line.
-	 */
-	public static void main(String[] args) {
-		System.out.print("Hello World!");
-	}
-	
-	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-	public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	}
+
+    /**
+     * Main method.
+     * Prints "Hello world!" via command line.
+     *
+     * @param   args    program arguments.
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+
+    /**
+     * Method echo.
+     *
+     * @param   name    Your name.
+     * @return          Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
