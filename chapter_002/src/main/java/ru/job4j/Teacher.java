ac1fc79edaec19ae67002f05641e583c5b64f6c2
vgurianov <37j5k5a@mail.ru>
2018-04-15 23:01:03 +0300

    Professions update

--- chapter_002/src/main/java/ru/job4j/Teacher.java
+++ chapter_002/src/main/java/ru/job4j/Teacher.java
@@ -3,7 +3,7 @@ package ru.job4j;
 /**
  * Created by User2 on 15.04.2018.
  */
-public class Teacher {
+public class Teacher extends Profession {
     public void teachStudent(Student student) {
 
     }
