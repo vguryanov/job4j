26d37cbd56d803b7fb5cc4a0fc60be6cf93f9626
vgurianov <37j5k5a@mail.ru>
2018-04-20 16:43:44 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -28,43 +28,35 @@ public class PaintTest {
         System.out.println("execute after method");
     }
 
-    public void drawToByteStream(Shape shape, String expected) {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
-        new Paint().draw(shape);
-        // проверяем результат вычисления
-        assertThat(new String(out.toByteArray()), is(expected));
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
-    }
-
     @Test
     public void whenDrawSquare() {
-        drawToByteStream(new Square(),
-                new StringBuilder()
-                        .append("+++++")
-                        .append("+++++")
-                        .append("+++++")
-                        .append(System.lineSeparator())
-                        .toString()
-
+        new Paint().draw(new Square());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("+++++")
+                                .append("+++++")
+                                .append("+++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
         );
     }
 
     @Test
     public void whenDrawTriangle() {
-        drawToByteStream(new Triangle(),
-                new StringBuilder()
-                        .append("  +  ")
-                        .append(" +++ ")
-                        .append("+++++")
-                        .append(System.lineSeparator())
-                        .toString()
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("  +  ")
+                                .append(" +++ ")
+                                .append("+++++")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
         );
     }
 }
