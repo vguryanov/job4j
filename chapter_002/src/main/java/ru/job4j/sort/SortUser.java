c265675929b41a3457ba80b2a4e55a687317c227
vgurianov <37j5k5a@mail.ru>
2018-04-27 13:33:45 +0300

    SortUser update

--- chapter_002/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort/SortUser.java
@@ -17,9 +17,7 @@ public class SortUser {
         users.sort(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                Integer o1NameLength = o1.getName().length();
-                Integer o2NameLength = o2.getName().length();
-                return o1NameLength.compareTo(o2NameLength);
+                return Integer.compare(o1.getName().length(), o2.getName().length());
             }
         });
 
