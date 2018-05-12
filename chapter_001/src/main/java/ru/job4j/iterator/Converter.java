dd5e2f6957729370cf4b29fef9d879d8016f4e03
vgurianov <37j5k5a@mail.ru>
2018-05-12 11:14:22 +0300

    Converter update

--- chapter_001/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_001/src/main/java/ru/job4j/iterator/Converter.java
@@ -22,23 +22,9 @@ public class Converter {
 
             @Override
             public Integer next() {
-                if (!currentIterator.hasNext()) {
-                    currentIterator = iterators.next();
-                }
+                hasNext();
                 return currentIterator.next();
             }
         };
-    }
-
-    public static void main(String[] args) {
-        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
-        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
-        Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
-        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        Iterator<Integer> it = new Converter().convert(its);
-
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
     }
 }
