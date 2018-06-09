46f6c00aade97a0789b02a84cd70760a6cf345df
vgurianov <37j5k5a@mail.ru>
2018-06-09 14:45:08 +0300

    ParallelSearchTest update

--- chapter_002/src/test/java/ru/job4j/concurrency/parallelsearch/ParallelSearchTest.java
+++ chapter_002/src/test/java/ru/job4j/concurrency/parallelsearch/ParallelSearchTest.java
@@ -2,6 +2,7 @@ package ru.job4j.concurrency.parallelsearch;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,15 +13,18 @@ import static org.hamcrest.core.Is.is;
  * Created by User2 on 01.06.2018.
  */
 public class ParallelSearchTest {
-//    @Test
-//    public void init() throws Exception {
-//        ParallelSearch ps = new ParallelSearch(
-//                "",
-//                "test",
-//                Arrays.asList(".txt")
-//        );
-//        ps.init();
+    @Test
+    public void init() throws Exception {
+        ParallelSearch ps = new ParallelSearch(
+                "",
+                "test",
+                Arrays.asList(".txt")
+        );
+        ps.init();
 //        List<String> expected = Arrays.asList("src\\test\\resources\\testfile.txt", "target\\test-classes\\testfile.txt");
-//        assertThat(ps.result(), is(expected));
-//    }
+        List<String> expected = Arrays.asList(
+                "src" + File.separator + "test" + File.separator + "resources" + File.separator + "testfile.txt",
+                "target" + File.separator + "test-classes" + File.separator + "testfile.txt");
+        assertThat(ps.result(), is(expected));
+    }
 }
