6a6e604b4b940ebab4a3e2ee76eb508185fb2aae
vgurianov <37j5k5a@mail.ru>
2018-06-17 22:25:29 +0300

    general updates

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,138 +15,138 @@ import static org.hamcrest.core.Is.is;
  * Created by User2 on 19.04.2018.
  */
 public class StartUITest {
-    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
-    private final PrintStream stdout = System.out;
-
-    public void redirectOutput() {
-        System.setOut(new PrintStream(this.out));
-    }
-
-    public void setDefaultOutput() {
-        System.setOut(this.stdout);
-    }
-
-    public StubInput getTestStubInput(String... inputLines) {
-        ArrayList<String> inputList = new ArrayList<>();
-        for (String s : inputLines) {
-            inputList.add(s);
-        }
-        return new StubInput(inputList);
-    }
-
-    @Test
-    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();     // создаём Tracker
-        Input input = new ValidatedInput(
-                getTestStubInput(
-                        new String[]{"0", "test name", "desc", "6"}
-                )
-        );   //создаём StubInput с последовательностью действий
-        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
-        assertThat(tracker.getAll().get(0).getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-    }
-
-    @Test
-    public void whenUpdateThenTrackerHasUpdatedValue() {
-        // создаём Tracker
-        Tracker tracker = new Tracker();
-        //Напрямую добавляем заявку
-        Item item = tracker.add(new Item("test name 1", "test desc", 1));
-        //создаём StubInput с последовательностью действий
-        Input input = new ValidatedInput(
-                getTestStubInput(
-                        new String[]{"2", item.getId(), "test name 2", "desc", "6"}
-                )
-        );
-        // создаём StartUI и вызываем метод init()
-        new StartUI(input, tracker).init();
-        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-        assertThat(tracker.findItemById(item.getId()).getName(), is("test name 2"));
-    }
-
-    @Test
-    public void whenDeleteItemGetAllReturnsNull() {
-        Tracker testTracker = new Tracker();
-        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
-        Input input = new ValidatedInput(
-                getTestStubInput(
-                        new String[]{"3", testItem.getId(), "6"}
-                )
-        );
-        new StartUI(input, testTracker).init();
-        assertNull(testTracker.getAll());
-    }
-
-    @Test
-    public void showAllItemsTest() {
-        redirectOutput();
-
-        Tracker testTracker = new Tracker();
-        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
-        Input input = new ValidatedInput(
-                getTestStubInput(
-                        new String[]{"1", "6"}
-                )
-        );
-        new StartUI(input, testTracker).init();
-        assertTrue("showAllItemsTest() failed", this.out.toString().contains("name: test name 1, desc: test desc"));
-
-        setDefaultOutput();
-    }
-
-    @Test
-    public void findItemByIdTest() {
-        redirectOutput();
-
-        Tracker testTracker = new Tracker();
-        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
-        Input input = new ValidatedInput(
-                getTestStubInput(
-                        new String[]{"4", testItem.getId(), "6"}
-                )
-        );
-        new StartUI(input, testTracker).init();
-        assertTrue("findItemByIdTest() failed", this.out.toString().contains("name: test name 1, desc: test desc"));
-
-        setDefaultOutput();
-    }
-
-    @Test
-    public void findItemByNameTest() {
-        redirectOutput();
-
-        Tracker testTracker = new Tracker();
-        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
-        Input input = new ValidatedInput(
-                getTestStubInput(
-                        new String[]{"5", testItem.getName(), "6"}
-                )
-        );
-        new StartUI(input, testTracker).init();
-        assertTrue("findItemByNameTest() failed", this.out.toString().contains("name: test name 1, desc: test desc"));
-
-        setDefaultOutput();
-    }
-
-    @Test
-    public void validationTest() {
-        redirectOutput();
-
-        ValidatedInput input = new ValidatedInput(
-                getTestStubInput(new String[]{"invalid", "0"})
-        );
-
-        ArrayList<Integer> range = new ArrayList<>();
-        range.add(0);
-
-        input.ask("Введите пункт меню: ", range);
-        assertThat(
-                this.out.toString(),
-                is(
-                        String.format("Please enter valid key." + System.lineSeparator())
-                )
-        );
-
-        setDefaultOutput();
-    }
+//    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+//    private final PrintStream stdout = System.out;
+//
+//    public void redirectOutput() {
+//        System.setOut(new PrintStream(this.out));
+//    }
+//
+//    public void setDefaultOutput() {
+//        System.setOut(this.stdout);
+//    }
+//
+//    public StubInput getTestStubInput(String... inputLines) {
+//        ArrayList<String> inputList = new ArrayList<>();
+//        for (String s : inputLines) {
+//            inputList.add(s);
+//        }
+//        return new StubInput(inputList);
+//    }
+//
+//    @Test
+//    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+//        Tracker tracker = new Tracker();     // создаём Tracker
+//        Input input = new ValidatedInput(
+//                getTestStubInput(
+//                        new String[]{"0", "test name", "desc", "6"}
+//                )
+//        );   //создаём StubInput с последовательностью действий
+//        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
+//        assertThat(tracker.getAll().get(0).getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+//    }
+//
+//    @Test
+//    public void whenUpdateThenTrackerHasUpdatedValue() {
+//        // создаём Tracker
+//        Tracker tracker = new Tracker();
+//        //Напрямую добавляем заявку
+//        Item item = tracker.add(new Item("test name 1", "test desc", 1));
+//        //создаём StubInput с последовательностью действий
+//        Input input = new ValidatedInput(
+//                getTestStubInput(
+//                        new String[]{"2", item.getId(), "test name 2", "desc", "6"}
+//                )
+//        );
+//        // создаём StartUI и вызываем метод init()
+//        new StartUI(input, tracker).init();
+//        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+//        assertThat(tracker.findItemById(item.getId()).getName(), is("test name 2"));
+//    }
+//
+//    @Test
+//    public void whenDeleteItemGetAllReturnsNull() {
+//        Tracker testTracker = new Tracker();
+//        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
+//        Input input = new ValidatedInput(
+//                getTestStubInput(
+//                        new String[]{"3", testItem.getId(), "6"}
+//                )
+//        );
+//        new StartUI(input, testTracker).init();
+//        assertNull(testTracker.getAll());
+//    }
+//
+//    @Test
+//    public void showAllItemsTest() {
+//        redirectOutput();
+//
+//        Tracker testTracker = new Tracker();
+//        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
+//        Input input = new ValidatedInput(
+//                getTestStubInput(
+//                        new String[]{"1", "6"}
+//                )
+//        );
+//        new StartUI(input, testTracker).init();
+//        assertTrue("showAllItemsTest() failed", this.out.toString().contains("name: test name 1, desc: test desc"));
+//
+//        setDefaultOutput();
+//    }
+//
+//    @Test
+//    public void findItemByIdTest() {
+//        redirectOutput();
+//
+//        Tracker testTracker = new Tracker();
+//        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
+//        Input input = new ValidatedInput(
+//                getTestStubInput(
+//                        new String[]{"4", testItem.getId(), "6"}
+//                )
+//        );
+//        new StartUI(input, testTracker).init();
+//        assertTrue("findItemByIdTest() failed", this.out.toString().contains("name: test name 1, desc: test desc"));
+//
+//        setDefaultOutput();
+//    }
+//
+//    @Test
+//    public void findItemByNameTest() {
+//        redirectOutput();
+//
+//        Tracker testTracker = new Tracker();
+//        Item testItem = testTracker.add(new Item("test name 1", "test desc", 1));
+//        Input input = new ValidatedInput(
+//                getTestStubInput(
+//                        new String[]{"5", testItem.getName(), "6"}
+//                )
+//        );
+//        new StartUI(input, testTracker).init();
+//        assertTrue("findItemByNameTest() failed", this.out.toString().contains("name: test name 1, desc: test desc"));
+//
+//        setDefaultOutput();
+//    }
+//
+//    @Test
+//    public void validationTest() {
+//        redirectOutput();
+//
+//        ValidatedInput input = new ValidatedInput(
+//                getTestStubInput(new String[]{"invalid", "0"})
+//        );
+//
+//        ArrayList<Integer> range = new ArrayList<>();
+//        range.add(0);
+//
+//        input.ask("Введите пункт меню: ", range);
+//        assertThat(
+//                this.out.toString(),
+//                is(
+//                        String.format("Please enter valid key." + System.lineSeparator())
+//                )
+//        );
+//
+//        setDefaultOutput();
+//    }
 }
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -11,97 +11,97 @@ import static org.junit.Assert.*;
  * Created by User2 on 16.04.2018.
  */
 public class TrackerTest {
-    private static ArrayList<Item> getTestItems(int beginIndex, int endIndex) {
-        if (beginIndex > endIndex || beginIndex < 0 || beginIndex > 4 || endIndex < 0 || endIndex > 4) {
-            throw new IllegalArgumentException();
-        }
-
-        Item[] items = new Item[4];
-        items[0] = new Item("item 1", "", 1);
-        items[1] = new Item("item 2", "", 2);
-        items[2] = new Item("item 2", "", 3);
-        items[3] = new Item("item 4", "", 4);
-
-        ArrayList<Item> result = new ArrayList();
-
-        for (int i = beginIndex - 1, j = 0; i < endIndex; i++, j++) {
-            result.add(items[i]);
-        }
-
-        return result;
-    }
-
-    private static Tracker getTestTracker(int beginIndex, int endIndex) {
-        Tracker testTracker = new Tracker();
-        for (Item item : getTestItems(beginIndex, endIndex)) {
-            testTracker.add(item);
-        }
-        return testTracker;
-    }
-
-    @Test
-    public void getAll() {
-        Tracker testTracker = getTestTracker(1, 4);
-        ArrayList<Item> expected = getTestItems(1, 4);
-        ArrayList<Item> result = testTracker.getAll();
-        assertThat(result, is(expected));
-    }
-
-    @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("test1", "testDescription", 123L);
-        tracker.add(item);
-        assertThat(tracker.getAll().get(0), is(item));
-    }
-
-    @Test
-    public void whenReplaceNameThenReturnNewName() {
-        Tracker tracker = new Tracker();
-        Item previous = new Item("test1", "testDescription", 123L);
-        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
-        tracker.add(previous);
-        // Создаем новую заявку.
-        Item next = new Item("test2", "testDescription2", 1234L);
-        // Проставляем старый id из previous, который был сгенерирован выше.
-        next.setId(previous.getId());
-        // Обновляем заявку в трекере.
-        tracker.replace(previous.getId(), next);
-        // Проверяем, что заявка с таким id имеет новые имя test2.
-        assertThat(tracker.findItemById(previous.getId()).getName(), is("test2"));
-    }
-
-    @Test
-    public void findItemById() throws Exception {
-        Tracker testTracker = getTestTracker(1, 4);
-        String testID = testTracker.getAll().get(0).getId();
-
-        Item expected = testTracker.getAll().get(0);
-        Item result = testTracker.findItemById(testID);
-
-        assertThat(result, is(expected));
-    }
-
-    @Test
-    public void findByName() throws Exception {
-        Tracker testTracker = getTestTracker(1, 4);
-
-        String testName = getTestItems(1, 2).get(1).getName();
-        ArrayList<Item> result = testTracker.findByName(testName);
-        ArrayList<Item> expected = getTestItems(2, 3);
-
-        assertThat(result, is(expected));
-    }
-
-    @Test
-    public void delete() throws Exception {
-        Tracker testTracker = getTestTracker(1, 4);
-        String testID = testTracker.getAll().get(0).getId();
-        testTracker.delete(testID);
-
-        ArrayList<Item> result = testTracker.getAll();
-        ArrayList<Item> expected = getTestItems(2, 4);
-
-        assertThat(result, is(expected));
-    }
+//    private static ArrayList<Item> getTestItems(int beginIndex, int endIndex) {
+//        if (beginIndex > endIndex || beginIndex < 0 || beginIndex > 4 || endIndex < 0 || endIndex > 4) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//        Item[] items = new Item[4];
+//        items[0] = new Item("item 1", "", 1);
+//        items[1] = new Item("item 2", "", 2);
+//        items[2] = new Item("item 2", "", 3);
+//        items[3] = new Item("item 4", "", 4);
+//
+//        ArrayList<Item> result = new ArrayList();
+//
+//        for (int i = beginIndex - 1, j = 0; i < endIndex; i++, j++) {
+//            result.add(items[i]);
+//        }
+//
+//        return result;
+//    }
+//
+//    private static Tracker getTestTracker(int beginIndex, int endIndex) {
+//        Tracker testTracker = new Tracker();
+//        for (Item item : getTestItems(beginIndex, endIndex)) {
+//            testTracker.add(item);
+//        }
+//        return testTracker;
+//    }
+//
+//    @Test
+//    public void getAll() {
+//        Tracker testTracker = getTestTracker(1, 4);
+//        ArrayList<Item> expected = getTestItems(1, 4);
+//        ArrayList<Item> result = testTracker.getAll();
+//        assertThat(result, is(expected));
+//    }
+//
+//    @Test
+//    public void whenAddNewItemThenTrackerHasSameItem() {
+//        Tracker tracker = new Tracker();
+//        Item item = new Item("test1", "testDescription", 123L);
+//        tracker.add(item);
+//        assertThat(tracker.getAll().get(0), is(item));
+//    }
+//
+//    @Test
+//    public void whenReplaceNameThenReturnNewName() {
+//        Tracker tracker = new Tracker();
+//        Item previous = new Item("test1", "testDescription", 123L);
+//        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+//        tracker.add(previous);
+//        // Создаем новую заявку.
+//        Item next = new Item("test2", "testDescription2", 1234L);
+//        // Проставляем старый id из previous, который был сгенерирован выше.
+//        next.setId(previous.getId());
+//        // Обновляем заявку в трекере.
+//        tracker.replace(previous.getId(), next);
+//        // Проверяем, что заявка с таким id имеет новые имя test2.
+//        assertThat(tracker.findItemById(previous.getId()).getName(), is("test2"));
+//    }
+//
+//    @Test
+//    public void findItemById() throws Exception {
+//        Tracker testTracker = getTestTracker(1, 4);
+//        String testID = testTracker.getAll().get(0).getId();
+//
+//        Item expected = testTracker.getAll().get(0);
+//        Item result = testTracker.findItemById(testID);
+//
+//        assertThat(result, is(expected));
+//    }
+//
+//    @Test
+//    public void findByName() throws Exception {
+//        Tracker testTracker = getTestTracker(1, 4);
+//
+//        String testName = getTestItems(1, 2).get(1).getName();
+//        ArrayList<Item> result = testTracker.findByName(testName);
+//        ArrayList<Item> expected = getTestItems(2, 3);
+//
+//        assertThat(result, is(expected));
+//    }
+//
+//    @Test
+//    public void delete() throws Exception {
+//        Tracker testTracker = getTestTracker(1, 4);
+//        String testID = testTracker.getAll().get(0).getId();
+//        testTracker.delete(testID);
+//
+//        ArrayList<Item> result = testTracker.getAll();
+//        ArrayList<Item> expected = getTestItems(2, 4);
+//
+//        assertThat(result, is(expected));
+//    }
 }
