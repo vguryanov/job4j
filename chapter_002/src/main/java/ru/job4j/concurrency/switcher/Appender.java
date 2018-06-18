8674ddd8b35ff30acc7dc9f0fa8c5b78dfac6158
vgurianov <37j5k5a@mail.ru>
2018-06-19 00:53:49 +0300

    Switcher update

--- chapter_002/src/main/java/ru/job4j/concurrency/switcher/Appender.java
+++ chapter_002/src/main/java/ru/job4j/concurrency/switcher/Appender.java
@@ -1,5 +1,7 @@
 package ru.job4j.concurrency.switcher;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by User2 on 14.06.2018.
  */
@@ -14,14 +16,15 @@ class Appender extends Thread {
 
     @Override
     public void run() {
+        ReentrantLock lock = switcher.getLock();
         while (!isInterrupted()) {
-            synchronized (switcher) {
+            synchronized (lock) {
                 for (int i = 0; i < 10; i++) {
                     switcher.append(value);
                 }
-                switcher.notifyAll();
+                lock.notifyAll();
                 try {
-                    switcher.wait();
+                    lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
--- chapter_002/src/main/java/ru/job4j/concurrency/switcher/Switcher.java
+++ chapter_002/src/main/java/ru/job4j/concurrency/switcher/Switcher.java
@@ -1,13 +1,20 @@
 package ru.job4j.concurrency.switcher;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by User2 on 14.06.2018.
  */
 public class Switcher {
     private final StringBuilder value;
+    private ReentrantLock lock = new ReentrantLock(true);
 
     public Switcher(int i) {
         this.value = new StringBuilder(String.valueOf(i));
+    }
+
+    public ReentrantLock getLock() {
+        return lock;
     }
 
     public void append(char c) {
